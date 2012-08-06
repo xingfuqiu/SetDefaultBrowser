148c570456429dc616d145cad8034b3cee1f76ea
xingfuqiu <xingfuqiu@gmail.com>
2012-08-06 15:17:47 +0800

    first

---
+++ Unit1.pas
@@ -0,0 +1,95 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Registry, StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Button2: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    function SetDefaultBrowser_Win7(GTBPath: String): Boolean;
+    function SetDefaultBrowser_XP(GTBPath: String): Boolean;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  SetDefaultBrowser_XP(Edit1.Text);
+end;
+
+//******************************************************************************
+// …Ë÷√ƒ¨»œ‰Ø¿¿∆˜ for win7
+//******************************************************************************
+function TForm1.SetDefaultBrowser_Win7(GTBPath: String): Boolean;
+var
+  reg: TRegistry;
+begin
+  Result := False;
+  reg := TRegistry.Create;
+  try
+    reg.RootKey := HKEY_CLASSES_ROOT;
+    if reg.OpenKey('GTBrowser\shell\open\command', True) then
+    begin
+      reg.WriteString('', GTBPath);
+    end;
+    reg.CloseKey;
+
+    reg.RootKey := HKEY_CURRENT_USER;
+    if reg.OpenKey('Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice', True) then
+    begin
+      reg.WriteString('Progid', 'GTBrowser');
+    end;
+    Result := True;
+  finally
+    reg.Free;
+  end;
+end;
+
+//******************************************************************************
+// …Ë÷√ƒ¨»œ‰Ø¿¿∆˜ for XP
+//******************************************************************************
+function TForm1.SetDefaultBrowser_XP(GTBPath: String): Boolean;
+var
+  reg: TRegistry;
+begin
+  Result := False;
+  reg := TRegistry.Create;
+  try
+    reg.RootKey := HKEY_CLASSES_ROOT;
+
+    if Not reg.OpenKey('http\shell\open\command', True) then Exit;
+    reg.WriteString('', GTBPath);
+    reg.CloseKey;
+
+    if Not reg.OpenKey('http\shell\open\ddeexec\Application', True) then Exit;
+    reg.WriteString('', 'GTBrowser');
+    reg.CloseKey;
+    Result := True;
+  finally
+    reg.Free;
+  end;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  SetDefaultBrowser_Win7(Edit1.Text);
+end;
+
+end.
---
+++ Unit1.~dfm
@@ -0,0 +1,50 @@
+object Form1: TForm1
+  Left = 528
+  Top = 552
+  Width = 875
+  Height = 233
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 104
+    Top = 104
+    Width = 137
+    Height = 65
+    Caption = #35774#32622'(XP)'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 104
+    Top = 40
+    Width = 697
+    Height = 21
+    TabOrder = 1
+    Text = 'F:\Work\Project\GTBrowser\WebGamegt\bin\WebGamegt.exe openurl %1'
+  end
+  object Edit2: TEdit
+    Left = 104
+    Top = 72
+    Width = 121
+    Height = 21
+    TabOrder = 2
+    Text = 'GTBrowser'
+  end
+  object Button2: TButton
+    Left = 256
+    Top = 104
+    Width = 137
+    Height = 65
+    Caption = #35774#32622'(Win7)'
+    TabOrder = 3
+    OnClick = Button2Click
+  end
+end
